2df87345f007fb0ec608f719a1f344aec68785da
huypq <huyphamquang1997@gmail.com>
2023-02-06 22:55:41 +0700

    add mangapage

---
+++ app/src/main/java/com/example/tachiyomi_clone/ui/page/MangaPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tachiyomi_clone.ui.page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tachiyomi_clone.R
+
+class MangaPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manga_page)
+    }
+}
