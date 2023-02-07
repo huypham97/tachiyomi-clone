802db98ee2e9f360f67562e6a0b6851fe0dbef72
adminn <sonnmph13824@fpt.edu.vn>
2023-02-07 15:26:43 +0700

    ui

---
+++ app/src/main/java/com/example/tachiyomi_clone/son_ui/Son_TrangChu.kt
@@ -0,0 +1,12 @@
+package com.example.tachiyomi_clone.son_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tachiyomi_clone.R
+
+class Son_TrangChu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_son_trang_chu)
+    }
+}
